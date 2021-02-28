ed4e319cfb37727e4a084c8aec490f216f3e9370
gh0st25 <gh0st25@317279.no-reply.drupal.org>
2021-02-28 10:54:49 -0500

    Issue #2963731 by FeyP, kevinquillen, saivaishnavijupudi, Giuseppe87: Add additional plugin options

--- src/Plugin/CKEditorPlugin/Wordcount.php
+++ src/Plugin/CKEditorPlugin/Wordcount.php
@@ -55,13 +55,17 @@ class Wordcount extends CKEditorPluginBase implements CKEditorPluginConfigurable
 
     return [
       'wordcount' => [
+        'showRemaining' => !empty($settings['plugins']['wordcount']['show_remaining']) ? $settings['plugins']['wordcount']['show_remaining'] : FALSE,
         'showParagraphs' => !empty($settings['plugins']['wordcount']['show_paragraphs']) ? $settings['plugins']['wordcount']['show_paragraphs'] : FALSE,
         'showWordCount' => !empty($settings['plugins']['wordcount']['show_word_count']) ? $settings['plugins']['wordcount']['show_word_count'] : FALSE,
         'showCharCount' => !empty($settings['plugins']['wordcount']['show_char_count']) ? $settings['plugins']['wordcount']['show_char_count'] : FALSE,
+        'countBytesAsChars' => !empty($settings['plugins']['wordcount']['count_bytes']) ? $settings['plugins']['wordcount']['count_bytes'] : FALSE,
         'countSpacesAsChars' => !empty($settings['plugins']['wordcount']['count_spaces']) ? $settings['plugins']['wordcount']['count_spaces'] : FALSE,
         'countHTML' => !empty($settings['plugins']['wordcount']['count_html']) ? $settings['plugins']['wordcount']['count_html'] : FALSE,
+        'countLineBreaks' => !empty($settings['plugins']['wordcount']['count_line_breaks']) ? $settings['plugins']['wordcount']['count_line_breaks'] : FALSE,
         'maxWordCount' => !empty($settings['plugins']['wordcount']['max_words']) ? $settings['plugins']['wordcount']['max_words'] : -1,
         'maxCharCount' => !empty($settings['plugins']['wordcount']['max_chars']) ? $settings['plugins']['wordcount']['max_chars'] : -1,
+        'hardLimit' => isset($settings['plugins']['wordcount']['hard_limit']) ? $settings['plugins']['wordcount']['hard_limit'] : TRUE,
       ],
     ];
   }
@@ -76,6 +80,12 @@ class Wordcount extends CKEditorPluginBase implements CKEditorPluginConfigurable
       '#type' => 'checkbox',
       '#title' => $this->t('Enable the counter'),
       '#default_value' => !empty($settings['plugins']['wordcount']['enable']) ? $settings['plugins']['wordcount']['enable'] : FALSE,
+    ];
+
+    $form['show_remaining'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show remaining'),
+      '#default_value' => !empty($settings['plugins']['wordcount']['show_remaining']) ? $settings['plugins']['wordcount']['show_remaining'] : FALSE,
     ];
 
     $form['show_paragraphs'] = [
@@ -96,6 +106,12 @@ class Wordcount extends CKEditorPluginBase implements CKEditorPluginConfigurable
       '#default_value' => !empty($settings['plugins']['wordcount']['show_char_count']) ? $settings['plugins']['wordcount']['show_char_count'] : FALSE,
     ];
 
+    $form['count_bytes'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Count bytes'),
+      '#default_value' => !empty($settings['plugins']['wordcount']['count_bytes']) ? $settings['plugins']['wordcount']['count_bytes'] : FALSE,
+    ];
+
     $form['count_spaces'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Count spaces as characters'),
@@ -106,6 +122,12 @@ class Wordcount extends CKEditorPluginBase implements CKEditorPluginConfigurable
       '#type' => 'checkbox',
       '#title' => $this->t('Count HTML as characters'),
       '#default_value' => !empty($settings['plugins']['wordcount']['count_html']) ? $settings['plugins']['wordcount']['count_html'] : FALSE,
+    ];
+
+    $form['count_line_breaks'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Count line breaks'),
+      '#default_value' => !empty($settings['plugins']['wordcount']['count_line_breaks']) ? $settings['plugins']['wordcount']['count_line_breaks'] : FALSE,
     ];
 
     $form['max_words'] = [
@@ -122,9 +144,14 @@ class Wordcount extends CKEditorPluginBase implements CKEditorPluginConfigurable
       '#default_value' => !empty($settings['plugins']['wordcount']['max_chars']) ? $settings['plugins']['wordcount']['max_chars'] : -1,
     ];
 
+    $form['hard_limit'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Lock editor when limit is reached'),
+      '#default_value' => isset($settings['plugins']['wordcount']['hard_limit']) ? $settings['plugins']['wordcount']['hard_limit'] : TRUE,
+    ];
+
     $form['max_words']['#element_validate'][] = [$this, 'isNumeric'];
     $form['max_chars']['#element_validate'][] = [$this, 'isNumeric'];
-
     return $form;
   }
 
